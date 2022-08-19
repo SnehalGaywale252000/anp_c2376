23c89734cdfb1c174e1c8d717167ab2bf0cca2f0
SnehalGaywale <101580672+SnehalGaywale@users.noreply.github.com>
2022-08-19 10:20:47 +0530

    Assignment1st

--- AssignmentFirst/src/org/example/model/Product.java
+++ AssignmentFirst/src/org/example/model/Product.java
@@ -1,5 +1,4 @@
 package org.example.model;
-
 public class Product {
   private int id;
   private String name;
--- AssignmentFirst/src/org/example/service/ProductService.java
+++ AssignmentFirst/src/org/example/service/ProductService.java
@@ -1,21 +1,15 @@
 package org.example.service;
 import org.example.model.Product;
-//import org.example.model.Product;
 public class ProductService {
-	
-	
-
-  public void increasePrice(Product p)
+	public void increasePrice(Product p)   
  
   {
      double newprice;
-	 
-	
-	 System.out.println(p.getPrice());
-	 newprice=p.getPrice()+100;
+	 System.out.println(p.getPrice());  //print the price in Product class
+	 newprice=p.getPrice()+100; //increase the price in 100 to the original price
    
  
-     System.out.println("newPrice"+newprice);
+     System.out.println("increase Price is"+newprice); //print the increase price
  
 	
   }
